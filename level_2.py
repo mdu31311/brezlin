8cffa8bcc79c9fc540744917a26691fae3fc83f1
Matthew <62810932+mdu31311@users.noreply.github.com>
2020-11-18 20:44:28 +0100

    Add files via upload

---
+++ level_2.py
@@ -0,0 +1,25 @@
+
+'''
+def n6_game_level_2():
+# this level should spawn a game play area 
+
+to advance, player must...
+1. battle track successfully
+	-fade/mist with animation?
+2. obtain 3 brezlin
+3. obtain 1 doner kebab
+4. be yelled at by 1 person
+5. finish one maze
+
+
+# include animations from https://github.com/peterbrittain/asciimatics/wiki
+#handle having enough brezlin to continue to next level
+if item_brezlin >= X:
+	n6_game_level_3:
+else:
+	print('you dont have enough brezlin')
+
+
+'''
+
+
