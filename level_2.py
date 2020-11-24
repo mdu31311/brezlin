03a845c8e4f1934a6bbb0553349d280a6a66e1e1
Matthew <62810932+mdu31311@users.noreply.github.com>
2020-11-24 14:56:29 +0100

    Update level_2.py

--- level_2.py
+++ level_2.py
@@ -1,25 +1,50 @@
 
 '''
-def n6_game_level_2():
-# this level should spawn a game play area 
-
 to advance, player must...
 1. battle track successfully
-	-fade/mist with animation?
+    -fade/mist with animation?
 2. obtain 3 brezlin
 3. obtain 1 doner kebab
 4. be yelled at by 1 person
 5. finish one maze
 
-
 # include animations from https://github.com/peterbrittain/asciimatics/wiki
 #handle having enough brezlin to continue to next level
-if item_brezlin >= X:
-	n6_game_level_3:
-else:
-	print('you dont have enough brezlin')
-
-
 '''
+
+import time
+from os import system
+import cmd
+import textwrap
+import sys
+import os
+from time import sleep
+import random
+###### asciimatics imports
+from asciimatics.screen import Screen
+from asciimatics.scene import Scene
+from asciimatics.effects import Cycle, Stars
+from asciimatics.renderers import FigletText
+
+
+
+def n6_game_level_2(screen):
+    effects = [
+        Cycle(
+            screen,
+            FigletText("LEVEL  2", font='big'),
+            screen.height // 2 - 8),
+        Cycle(
+            screen,
+            FigletText("GET  IT", font='big'),
+            screen.height // 2 + 3),
+        Stars(screen, (screen.width + screen.height) // 2)
+    ]
+    screen.play([Scene(effects, 500)])
+    
+Screen.wrapper(n6_game_level_2) # whole game jumps here. fix this
+
+
+
 
 
